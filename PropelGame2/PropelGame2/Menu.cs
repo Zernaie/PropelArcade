315ffd42417b87405f425fa37bb1fab928685a41
unknown <evanaterx@gmail.com>
2022-09-27 10:12:08 -0500

    initializing repo

---
+++ PropelGame2/PropelGame2/Menu.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Text;
+using static System.Console;
+namespace menuTest
+{   
+    class Menu
+    {
+        private int selectedIndex;
+        private string[] Options;
+        private string Prompt;
+
+        public Menu(string prompt, string[] options)
+        {
+            selectedIndex = 0;
+            Options = options;
+            Prompt = prompt;
+        }
+        public void renderBoard()
+        {
+            int width = (Console.BufferWidth - 3);
+            int height = (Console.BufferHeight - 3);
+ 
+            for (int i = 1; i <= (width + 2); i++)
+            {
+                Console.SetCursorPosition(i, 1);
+                Console.Write("#");
+            }
+            for (int i = 1; i <= (width + 2); i++)
+            {
+                Console.SetCursorPosition(i, (height + 2));
+                Console.Write("#");
+            }
+            for (int i = 1; i <= (height + 1); i++)
+            {
+                Console.SetCursorPosition(1, i);
+                Console.Write("#");
+            }
+            for (int i = 1; i <= (height + 1); i++)
+            {
+                Console.SetCursorPosition((width + 2), i);
+                Console.Write("#");
+            }
+            Console.CursorVisible = false;
+        }
+
+        private void DisplayOptions()
+        {
+            for(int i = 0; i < Options.Length; i++)
+            {
+                string currentOption = Options[i];
+                string prefix;
+
+                if (i == selectedIndex)
+                {
+                    prefix = $"{i}";
+                    ForegroundColor = ConsoleColor.Black;
+                    BackgroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    prefix = $"{i}";
+                    ForegroundColor = ConsoleColor.White;
+                    BackgroundColor = ConsoleColor.Black;
+                }
+                SetCursorPosition(5, 5 + i);
+                WriteLine($"{prefix} < {currentOption} >");
+            }
+            ResetColor();
+        }
+
+        public int runMenu()
+        {
+            ConsoleKey keyPressed;
+            do
+            {
+                renderBoard();
+                DisplayOptions();
+                SetCursorPosition(5, 3);
+                WriteLine(Prompt);
+
+                ConsoleKeyInfo keyInfo = ReadKey(true);
+                keyPressed = keyInfo.Key;
+
+                if (keyPressed == ConsoleKey.UpArrow)
+                {
+                    selectedIndex--; 
+                    if(selectedIndex == -1)
+                    {
+                        selectedIndex = Options.Length - 1;
+                    }
+                }
+                else if (keyPressed == ConsoleKey.DownArrow)
+                {
+                    selectedIndex++;
+                    if (selectedIndex == Options.Length)
+                    {
+                        selectedIndex = 0;
+                    }
+                }
+            } while (keyPressed != ConsoleKey.Enter);
+            return selectedIndex;
+        }
+    }
+}
---
+++ PropelGame2/PropelGame2/mainGame.cs
@@ -0,0 +1,84 @@
+﻿using menuTest;
+using System;
+using System.Security.Cryptography.X509Certificates;
+using static System.Console;
+
+namespace propelGameMain 
+{
+    class startMainGame
+    {
+        public void StartGame()
+        {
+            RunMainMenu();
+        }
+
+        private void RunMainMenu()
+        {
+            Clear();
+            string prompt = "Select a option (arrows+enter)";
+            string[] options = { "    Game 1    ", "    Test 1    ", "    Test 2    ", "     Exit     " };
+            Menu mainMenu = new Menu(prompt, options);
+            int selectedIndex = mainMenu.runMenu();
+
+            SetCursorPosition(4, 12);
+            switch (selectedIndex)
+            {
+                case 0:
+                    Game1();
+                    break;
+                case 1:
+                    Game2();
+                    break;
+                case 2:
+                    Game3();
+                    break;
+                case 3:
+                    ExitMenu();
+                    break;
+                default:
+                    RunMainMenu();
+                    break;
+            }
+        }
+
+        private void Game1()
+        {
+            WriteLine("there is no game yet.");
+            ReadKey();
+            RunMainMenu();
+        }
+        private void Game2()
+        {
+            WriteLine("there is no game yet.");
+            ReadKey();
+            RunMainMenu();
+        }
+        private void Game3()
+        {
+            WriteLine("there is no game yet.");
+            ReadKey();
+            RunMainMenu();
+        }
+
+        private void ExitMenu()
+        {
+            Environment.Exit(0);
+        }
+
+    }
+
+    class main
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WindowWidth = 100;
+            Console.WindowHeight = 30;
+            Console.BufferWidth = 100;
+            Console.BufferHeight = 30;
+
+            startMainGame gameTest = new startMainGame();
+            gameTest.StartGame();
+        }
+    }
+}
